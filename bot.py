573564e8435b066f84a6c5f120b52ce783f6d277
Comet <u3622245@connect.hku.hk>
2024-06-01 21:57:21 +0800

    remove hardcode and use .properties

--- bot.py
+++ bot.py
@@ -3,13 +3,19 @@ from discord.ext import commands
 from RCON import python_rcon_client
 import subprocess
 
+#load properties
+with open("bot.properties") as properties:
+    lines = properties.readlines()
+    bot_token = lines[0].split("=")[1].strip()
+    bot_prefix = lines[1].split("=")[1].strip()
+    server_ip = lines[2].split("=")[1].strip()
+    server_RCON_port = int(lines[3].split("=")[1].strip())
+    server_RCON_passsword = lines[4].split("=")[1].strip()
+    launch_path = lines[5].split("=")[1].strip()
+
 #initialize bot+
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix = "!", intents = intents)
-
-server_ip = "localhost"
-server_RCON_port = "25575"
-server_RCON_passsword = "12345678"
+bot = commands.Bot(command_prefix = bot_prefix, intents = intents)
 
 @bot.event
 async def on_ready():
@@ -22,11 +28,11 @@ async def Hello(ctx, *, message: str):
 
 @bot.command()
 async def command(ctx, *, message: str):
-    with python_rcon_client.RCONClient('localhost', 25575, '12345678') as rcon_client:
+    with python_rcon_client.RCONClient(server_ip, server_RCON_port, server_RCON_passsword) as rcon_client:
         rcon_client.command(message)
 
 @bot.command()
 async def start(ctx):
-    subprocess.run([r"path"])
+    subprocess.run([r"f{launch_path}"])
 
-bot.run("bot token")
+bot.run(bot_token)
